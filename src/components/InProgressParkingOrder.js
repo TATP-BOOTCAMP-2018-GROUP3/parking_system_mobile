7b51f0441f0341546ed1aa6f69b5eab3f1115b30
Skittle Wan <skittle.wan@oocl.com>
2018-12-07 17:47:49 +0800

    [Skittle] feat: create dummy in progress parking order page

---
+++ src/components/InProgressParkingOrder.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import 'antd-mobile/dist/antd-mobile.css';
+import { Grid } from 'antd-mobile';
+import { Button } from 'antd-mobile';
+import { WhiteSpace } from 'antd-mobile';
+
+export default class InProgressParkingOrder extends Component {
+
+    render() {
+        return (
+            <div>
+                asdsaas
+            </div>
+        )
+    }
+}
