053491ded33869a23f95a289f0cdd5b96e5d34ae
ooclalexcheung <alex.cheung@oocl.com>
2018-12-07 17:59:18 +0800

    [Alex] feat: implement Welcome Page Login UI

--- src/containers/WelcomesPageContainer.js
+++ src/containers/WelcomesPageContainer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import 'antd-mobile/dist/antd-mobile.css';
-import { Button } from 'antd-mobile';
-import { WhiteSpace } from 'antd-mobile';
+import { List, InputItem, Switch, Stepper, Slider, Radio, Checkbox, TextareaItem, WingBlank, WhiteSpace, Button } from 'antd-mobile';
+import { createForm } from 'rc-form';
 
 export default class WelcomesPageContainer extends Component {
     sendSimpleResponseToBackEnd = () => {
@@ -18,9 +18,28 @@ export default class WelcomesPageContainer extends Component {
     }
 
     render() {
+        const { getFieldProps } = this.props.form;
         return (
             <div>
                 <h1>Welcome!</h1>
+                
+                <List>
+                    {/* <List>表单输入项</List> */}
+                    <List>
+                        <InputItem
+                            {...getFieldProps('input3', {
+                                initialValue: 'User',
+                            })}
+                            clear
+                            placeholder="User"
+                        >User</InputItem>
+                        <InputItem
+                            {...getFieldProps('input4')}
+                            clear
+                            placeholder="请输入密码"
+                        >Password</InputItem>
+                    </List>
+                </List>
                 <Button type="primary" onClick={this.directToCustomerPageContainer}>Login</Button><WhiteSpace />
                 <Button type="primary" onClick={this.sendSimpleResponseToBackEnd}>Testing for simple request from BE</Button><WhiteSpace />
 
@@ -29,3 +48,4 @@ export default class WelcomesPageContainer extends Component {
         )
     }
 }
+WelcomesPageContainer = createForm()(WelcomesPageContainer);
