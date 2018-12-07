abf296ae4a8f04590ad2a96b59a277a8ec493b76
Eddie Fong <fonged@oocl.com>
2018-12-07 15:28:05 +0800

    [Eddie] feat: WelcomePage with useless button

---
+++ src/components/WelcomesPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class WelcomesPage extends Component {
+  render() {
+    return (
+      <div>
+          <h1>Welcome!</h1>
+          <button>Login</button>
+      </div>
+    )
+  }
+}
