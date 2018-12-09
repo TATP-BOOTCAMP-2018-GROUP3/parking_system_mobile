4468af5ea6455e65eb8a9f8c0d33679959680e26
Eddie Fong <fonged@oocl.com>
2018-12-09 12:21:40 +0800

    [Eddie] feat: add ParkingPage and FetchingPage with interaction with footer

---
+++ src/containers/FetchingPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class FetchingPage extends Component {
+    render() {
+        return (
+            <div>
+                It is fetching page
+      </div>
+        )
+    }
+}
---
+++ src/containers/ParkingPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ParkingPage extends Component {
+    render() {
+        return (
+            <div>
+                It is parking pages.
+      </div>
+        )
+    }
+}
