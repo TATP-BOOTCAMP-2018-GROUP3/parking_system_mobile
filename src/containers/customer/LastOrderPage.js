af674e87d07f13166192e7402e12419440d1e4af
ooclalexcheung <alex.cheung@oocl.com>
2018-12-10 15:49:30 +0800

    [Alex] feat: implement get pdf after submit request

--- src/containers/customer/LastOrderPage.js
+++ src/containers/customer/LastOrderPage.js
@@ -1,17 +1,38 @@
 import React, { Component } from 'react';
-import { WhiteSpace, WingBlank, Result, Icon } from 'antd-mobile';
+import { WhiteSpace, WingBlank, Result, Icon, Button } from 'antd-mobile';
+import * as jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 class LastOrderPage extends Component {
+
+    saveAsPdf = () => {
+        const input = document.getElementsByClassName("save-pdf-component")[0];
+        html2canvas(input)
+          .then((canvas) => {
+            const imgData = canvas.toDataURL('image/png');
+            const pdf = new jsPDF("portrait", "mm", [297+16, 210]);
+            pdf.addImage(imgData, 'JPEG', -150, 0);
+            
+            pdf.save("ticket-" + this.props.orderId + ".pdf");
+          })
+        ;
+    }
 
     render() {
         return (
             <WingBlank size="md">
                 <WhiteSpace size="sm" />
                 <Result
+                    className="save-pdf-component" 
                     img={<Icon type="check-circle" style={{ fill: '#21ba45', width: '60px', height: '60px' }} />}
                     title="Order Submmited!"
                     message= {"Your order id is: " +  this.props.orderId + ". Please wait for our staff to contact you."}
                 />
+
+                <WhiteSpace size="md" />
+                <WingBlank size="md">
+                    <Button className="greenButton" onClick={this.saveAsPdf}>Save as PDF</Button>
+                </WingBlank>
             </WingBlank>
         )
     }
